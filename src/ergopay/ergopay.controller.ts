c6ed73e57223cd098d718d0054da553742de274d
Shishir Pai <64056966+mgpai22@users.noreply.github.com>
2023-08-06 00:00:28 -0400

    support for address registration

--- src/ergopay/ergopay.controller.ts
+++ src/ergopay/ergopay.controller.ts
@@ -6,12 +6,30 @@ export class ErgoPayController {
   constructor(private readonly ergoPayService: ErgoPayService) {}
 
   @Get('generateShortLink/:base64Data')
-  async setBlock(
+  async generateShortLink(
     @Param('base64Data') base64Data: string,
   ): Promise<{ shortCode: string }> {
     const shortCode = await this.ergoPayService.generateErgoPayShortLink(
       base64Data,
     );
     return { shortCode: shortCode };
+  }
+
+  @Get('generateAddressLink/:uuid/:address')
+  async generateAddressLink(
+    @Param('uuid') uuid: string,
+    @Param('address') address: string,
+  ): Promise<{ shortCode: string }> {
+    const shortCode = await this.ergoPayService.createAddressLink(
+      uuid,
+      address,
+    );
+    return { shortCode: shortCode };
+  }
+
+  @Get('address/:uuid')
+  async getAddress(@Param('uuid') uuid: string): Promise<{ address: string }> {
+    const shortCode = await this.ergoPayService.getAddress(uuid);
+    return { address: shortCode };
   }
 }
--- src/ergopay/ergopay.service.ts
+++ src/ergopay/ergopay.service.ts
@@ -6,6 +6,7 @@ import {
   LINK_SHORTNER_API_KEY,
   LINK_SHORTNER_BACKEND_URL,
 } from '../api/api';
+import { ErgoAddress } from '@fleet-sdk/core';
 
 @Injectable()
 export class ErgoPayService {
@@ -13,15 +14,6 @@ export class ErgoPayService {
     basePath: EXPLORER_API_URL(),
   });
   private readonly explorerClient = DefaultApiFactory(this.explorerConf);
-  getHello(): string {
-    return 'Hello World!';
-  }
-
-  private isBase64(str: string): boolean {
-    const base64Regex =
-      /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/;
-    return base64Regex.test(str);
-  }
   async generateErgoPayShortLink(base64Txn: string): Promise<string> {
     try {
       const res = await axios.post(
@@ -39,6 +31,48 @@ export class ErgoPayService {
         },
       );
       return res.data.shortCode;
+    } catch (error) {
+      return 'null';
+    }
+  }
+
+  async createAddressLink(uuid: string, address: string): Promise<string> {
+    if (!ErgoAddress.validate(address)) {
+      return 'null';
+    }
+    try {
+      const res = await axios.post(
+        `${LINK_SHORTNER_BACKEND_URL()}/rest/v3/short-urls`,
+        {
+          longUrl: `{"address":"${address}"}`,
+          findIfExists: false,
+          validateUrl: false,
+          forwardQuery: true,
+          customSlug: uuid,
+        },
+        {
+          headers: {
+            'x-api-key': `${LINK_SHORTNER_API_KEY()}`,
+          },
+        },
+      );
+      return res.data.shortCode;
+    } catch (error) {
+      return 'null';
+    }
+  }
+
+  async getAddress(uuid: string): Promise<string> {
+    try {
+      const res = await axios.get(
+        `${LINK_SHORTNER_BACKEND_URL()}/rest/v3/short-urls/${uuid}`,
+        {
+          headers: {
+            'x-api-key': `${LINK_SHORTNER_API_KEY()}`,
+          },
+        },
+      );
+      return JSON.parse(res.data.longUrl).address;
     } catch (error) {
       return 'null';
     }
