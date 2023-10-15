ad55816134fea2431e36dc238367a550b45745cf
mgpai <64056966+mgpai22@users.noreply.github.com>
2023-10-15 04:04:06 -0400

    auth integration with supabase

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,25 +1,145 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpStatus,
+  HttpException,
+  Get,
+} from '@nestjs/common';
 import { AuthService, Signature } from './auth.service';
+import { SupabaseService } from '../supabase/supabase.service';
+const jwt = require('jsonwebtoken');
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly supabaseService: SupabaseService,
+  ) {}
 
   @Post('login')
   async authenticate(
-    @Body() body: { signature: Signature; address: string },
+    @Body() body: { signature: Signature; nonce: string; address: string },
   ): Promise<any> {
-    const { signature, address } = body;
-    const token: string | boolean = await this.authService.verifySignature(
+    const { signature, nonce, address } = body;
+    const verificationStatus: boolean = this.authService.verifySignature(
       signature,
       address,
     );
-    const status =
-      typeof token === 'string'
-        ? { status: 'Ok', token, address }
-        : { status: 'Reject' };
+    if (!verificationStatus) {
+      throw new HttpException(
+        'address verification failed',
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+    const supabase = this.supabaseService.getClient();
+
+    let userData;
+    let user;
+    try {
+      userData = await this.authService.getUserData(supabase, address);
+      user = userData.user_id;
+    } catch (e) {
+      throw new HttpException(
+        'error fetching user',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    if (userData.auth.genNonce !== nonce) {
+      throw new HttpException('nonce does not match', HttpStatus.UNAUTHORIZED);
+    }
+
+    if (!user) {
+      try {
+        user = await this.authService.createUser(supabase, address);
+      } catch (e) {
+        throw new HttpException(
+          'error creating user',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+
+    try {
+      await this.authService.updateUserData(supabase, address, user);
+    } catch (e) {
+      throw new HttpException(
+        'error updating user',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+
+    const token = jwt.sign(
+      {
+        address: address, // this will be read by RLS policy
+        sub: user.id,
+        aud: 'authenticated',
+      },
+      'my-secret-key',
+      { expiresIn: 60 * 2 },
+    );
+
     return {
-      status,
+      authToken: token,
     };
+  }
+
+  @Get('hello')
+  getHello(): { response: string } {
+    return { response: 'hello' };
+  }
+
+  @Post('nonce')
+  async nonce(@Body() body: { address: string }): Promise<any> {
+    const { address } = body;
+    const nonce = Math.floor(Math.random() * 1000000).toString();
+    const supabase = this.supabaseService.getClient();
+
+    const { data, error } = await supabase
+      .from('users')
+      .select('address')
+      .eq('address', address);
+
+    if (error || data.length === 0) {
+      // Address not found, create new entry
+      const { error: insertError } = await supabase.from('users').insert({
+        address: address,
+        auth: {
+          genNonce: nonce,
+          lastAuth: new Date().toISOString(),
+          lastAuthStatus: 'pending',
+        },
+      });
+
+      if (insertError) {
+        console.log(insertError);
+        throw new HttpException(
+          'Error creating new entry',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    } else {
+      // Address found, update the entry
+      const updateRes = await supabase
+        .from('users')
+        .update({
+          auth: {
+            genNonce: nonce,
+            lastAuth: new Date().toISOString(),
+            lastAuthStatus: 'pending',
+          },
+        })
+        .eq('address', address);
+
+      if (updateRes.error) {
+        throw new HttpException(
+          'Error updating nonce',
+          HttpStatus.INTERNAL_SERVER_ERROR,
+        );
+      }
+    }
+
+    return { nonce };
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Address, verify_signature } from 'ergo-lib-wasm-nodejs';
 import { ErgoAddress, Network } from '@fleet-sdk/core';
+import { SupabaseClient } from '@supabase/supabase-js';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const jwt = require('jsonwebtoken');
 
@@ -14,7 +15,7 @@ export class AuthService {
   private readonly TIMESTAMP: number = 2;
   private readonly ORIGIN: number = 1;
 
-  verifySignature(signature: Signature, address: string): boolean | string {
+  verifySignature(signature: Signature, address: string): boolean {
     try {
       const arr: string[] = signature.signedMessage.split(';');
 
@@ -25,7 +26,7 @@ export class AuthService {
       const timestamp = parseInt(arr[this.TIMESTAMP], 10) * 1000;
       const origin = arr[this.ORIGIN];
 
-      const moreThen10SecPassed = timestamp < Date.now() - 1000 * 15;
+      const moreThen10SecPassed = false;
 
       if (moreThen10SecPassed) {
         return false;
@@ -36,21 +37,66 @@ export class AuthService {
           ? Address.from_mainnet_str(address)
           : Address.from_testnet_str(address);
 
-      const isVerified = verify_signature(
+      return verify_signature(
         networkAddress,
         Buffer.from(signature.signedMessage, 'utf-8'),
         Buffer.from(signature.proof, 'hex'),
       );
-
-      if (isVerified) {
-        return jwt.sign({ address }, '', {
-          expiresIn: '10d',
-        });
-      } else {
-        return false;
-      }
     } catch (e) {
+      console.log(e);
       return false;
+    }
+  }
+
+  async getUserData(
+    supabase: SupabaseClient<any, 'public', any>,
+    address: string,
+  ): Promise<string> {
+    const { data, error } = await supabase
+      .from('users')
+      .select('*')
+      .eq('address', address);
+    if (error) {
+      throw error;
+    }
+    return data[0];
+  }
+
+  async createUser(
+    supabase: SupabaseClient<any, 'public', any>,
+    address: string,
+  ): Promise<string> {
+    const { data, error } = await supabase.auth.admin.createUser({
+      email: `${address}@ergoplatform.com`,
+      user_metadata: { address: address },
+    });
+    if (error) {
+      throw error;
+    }
+    return data.user.id;
+  }
+
+  async updateUserData(
+    supabase: SupabaseClient<any, 'public', any>,
+    address: string,
+    user: string,
+  ): Promise<void> {
+    const newNonce = Math.floor(Math.random() * 1000000).toString();
+
+    const { data: updatedData, error: updateError } = await supabase
+      .from('users')
+      .update({
+        auth: {
+          genNonce: newNonce, // update the nonce, so it can't be reused
+          lastAuth: new Date().toISOString(),
+          lastAuthStatus: 'success',
+        },
+        user_id: user, // same uuid as auth.users table
+      })
+      .eq('address', address); // primary key
+
+    if (updateError) {
+      throw updateError;
     }
   }
 }
