7cc9e8a627e636bebf92c4f51d1ecd4de5099c02
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-12-28 21:08:32 -0500

    refactor(blocks): remove unused blocks module and related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { BlocksModule } from './blocks/blocks.module';
 import { GatewayModule } from './blocks-wss/gateway.module';
 import { ErgoPayModule } from './ergopay/ergopay.module';
 import { ConfigModule } from '@nestjs/config';
